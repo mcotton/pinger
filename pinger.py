2d11c4d1cfc1bf6acbdd3db17f9cf589112cfb34
mcotton <none@mcottondesign.com>
2012-05-30 09:38:35 -0500

    Improved output
    Output now includes site name, failed pings include both name and ip

--- pinger.py
+++ pinger.py
@@ -1,16 +1,26 @@
 import os
 import time
 
-sites = ['192.168.1.1','192.168.2.1','192.168.3.1','192.168.4.1','192.168.5.1','192.168.6.1','192.168.7.1','192.168.8.1','192.168.9.1', '192.168.10.5']
+sites = [{'name': 'Greco', 'ip': '192.168.1.1'},
+         {'name': 'New Braunfels', 'ip': '192.168.2.1'},
+         {'name': 'Floresville', 'ip': '192.168.3.1'},
+         {'name': 'Billing', 'ip': '192.168.4.1'},
+         {'name': 'Pleasanton', 'ip': '192.168.5.1'},
+         {'name': 'Judson', 'ip': '192.168.6.1'},
+         {'name': 'Poth', 'ip': '192.168.7.1'},
+         {'name': 'Wellness Center', 'ip': '192.168.8.1'},
+         {'name': 'Seguin', 'ip': '192.168.9.1'},
+         {'name': 'RackSpace', 'ip': '192.168.10.3'}]
+
 
 def pinger(sites):
 	results = '\n************** RESULTS *************************\n\n'
 
 	for s in sites:
-		if os.system('ping ' + s) == 0:
-			results = results + '\t' + s + ' is up\n'
+		if os.system('ping ' + s['ip']) == 0:
+			results = results + '\t' + s['name'] + ' is up\n'
 		else:
-			results = results + '\t' + s + ' is down\n'
+			results = results + '\t' + s['name'] + '(' + s['ip'] + ')' + ' is down\n'
 
 	print results + '\tClosing in 20 seconds.'
 
